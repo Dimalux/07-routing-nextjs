31dc11638190e2bf33880f422408aea2de097ad9
Dima Vujchich <astrallux777@ukr.net>
2025-08-25 22:27:33 +0300

    Initial commit

---
+++ types/note.ts
@@ -0,0 +1,16 @@
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface NewNote {
+  title: string;
+  content: string;
+  tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+}
+
+
